19fd1fe7e134c262474aea2e8663598ba427e873
Mehow <kaiser.mehow@gmail.com>
2022-12-25 13:21:15 +0000

    resize for mobile
    https://stackoverflow.com/questions/66191191/how-to-create-a-responsive-game-for-any-screen-size-with-phaser-3
    
    https://labs.phaser.io/edit.html?src=src/scalemanager/mobile%20game%20example.js

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -294,6 +294,9 @@ class BadGuys extends Phaser.Physics.Arcade.Group {
 }
 
 export default class Demo extends Phaser.Scene {
+  GAME_WIDTH = 800;
+  GAME_HEIGHT = 600;
+
   badguys;
   button: Phaser.GameObjects.Text | null;
   mode: string = "menu";
@@ -328,6 +331,23 @@ export default class Demo extends Phaser.Scene {
   }
 
   create() {
+    const width = this.scale.gameSize.width;
+    const height = this.scale.gameSize.height;
+    this.parent = new Phaser.Structs.Size(width, height);
+    this.sizer = new Phaser.Structs.Size(
+      this.GAME_WIDTH,
+      this.GAME_HEIGHT,
+      Phaser.Structs.Size.FIT,
+      this.parent
+    );
+
+    this.parent.setSize(width, height);
+    this.sizer.setSize(width, height);
+
+    this.updateCamera();
+
+    this.scale.on("resize", this.resize, this);
+
     this.anims.create({
       key: "explode",
       frames: this.anims.generateFrameNumbers("explosion_img", {
@@ -355,7 +375,7 @@ export default class Demo extends Phaser.Scene {
       this.mode = "play";
       this.button?.setVisible(false);
     });
-    this.physics.world.setBounds(0, 0, 800, 600);
+    this.physics.world.setBounds(0, 0, this.GAME_WIDTH, this.GAME_HEIGHT);
     const gunPosition = { x: 700, y: 500 };
     const bullets = new Bullets(this);
     this.input.addPointer(5);
@@ -467,6 +487,33 @@ export default class Demo extends Phaser.Scene {
         snore.stop();
       }
     });
+  }
+
+  resize(gameSize) {
+    const width = gameSize.width;
+    const height = gameSize.height;
+
+    this.parent.setSize(width, height);
+    this.sizer.setSize(width, height);
+
+    this.updateCamera();
+  }
+
+  updateCamera() {
+    const camera = this.cameras.main;
+
+    const x = Math.ceil((this.parent.width - this.sizer.width) * 0.5);
+    const y = 0;
+    const scaleX = this.sizer.width / this.GAME_WIDTH;
+    const scaleY = this.sizer.height / this.GAME_HEIGHT;
+
+    camera.setViewport(x, y, this.sizer.width, this.sizer.height);
+    camera.setZoom(Math.max(scaleX, scaleY));
+    camera.centerOn(this.GAME_WIDTH / 2, this.GAME_HEIGHT / 2);
+  }
+
+  getZoom() {
+    return this.cameras.main.zoom;
   }
 
   update(time, delta) {
